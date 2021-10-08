2dc91f4b60f9fcc0c6c8b5b01cc9a8cc90525194
Matt Rumschlag <matt.rumschlag@ey.com>
2021-10-08 14:32:02 -0400

    updates

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const path = require('path');
 const { readdirSync } = require('fs')
 
 const getDirectories = source =>
-  readdirSync(source, { withFileTypes: true })
+  readdirSync(path.resolve(__dirname, source), { withFileTypes: true })
     .filter(dirent => dirent.isDirectory())
     .map(dirent => dirent.name)
 
@@ -13,6 +14,8 @@ try {
   console.log(`Getting files from  ${parentDir}!`);
   const dirs = getDirectories(parentDir);
   core.setOutput("dirs", JSON.stringify(dirs));
+  core.setOutput("hasData", (dirs.length > 0));
+  core.setOutput("directoryCount", dirs.length);
 
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
