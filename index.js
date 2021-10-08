0380a0d67c7e3020046f7ddf96ba2cc7a26c2c08
Matt Rumschlag <matt.rumschlag@ey.com>
2021-10-08 14:24:17 -0400

    updated app

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { readdirSync } = require('fs')
+
+const getDirectories = source =>
+  readdirSync(source, { withFileTypes: true })
+    .filter(dirent => dirent.isDirectory())
+    .map(dirent => dirent.name)
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
-  console.log(`Hello ${nameToGreet}!`);
-  const time = (new Date()).toTimeString();
-  core.setOutput("time", time);
+  const parentDir = core.getInput('parentDir');
+  console.log(`Getting files from  ${parentDir}!`);
+  const dirs = getDirectories(parentDir);
+  core.setOutput("dirs", JSON.stringify(dirs));
+
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
