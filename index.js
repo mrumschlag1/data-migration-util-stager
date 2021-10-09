f232b6d24311c6d7c4b16ad4fb53d99848bc189b
Matt Rumschlag <matt.rumschlag@ey.com>
2021-10-09 15:05:52 -0400

    yaml

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const path = require('path');
-const { readdirSync } = require('fs')
+const { readdirSync, existsSync } = require('fs')
 
 const getDirectories = source =>
   readdirSync(source, { withFileTypes: true })
@@ -9,24 +9,16 @@ const getDirectories = source =>
     .map(dirent => dirent.name)
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  // Get the JSON webhook payload for the event that triggered the workflow
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-
-  console.log('ACTION: ' + process.env['GITHUB_ACTION_PATH']);
-  console.log('WORKSPACE: ' + process.env['GITHUB_WORKSPACE']);
-  console.log('RUNNER: ' + process.env['RUNNER_NAME']);
-  console.log('HOME: ' + process.env['HOME']);
-  console.log(JSON.stringify(process.env));
-  
   const parentDir = core.getInput('parentDir');
-  console.log(`Getting files from  ${parentDir}!`);
-  const dirs = getDirectories(parentDir);
-  core.setOutput("dirs", JSON.stringify(dirs));
-  core.setOutput("hasData", (dirs.length > 0));
-  core.setOutput("directoryCount", dirs.length);
-
+  if(existsSync(parentDir)){
+      console.log(`Getting files from  ${parentDir}!`);
+      const dirs = getDirectories(process.env['GITHUB_WORKSPACE'] + '/' + parentDir);
+      core.setOutput("dirs", JSON.stringify(dirs));
+      core.setOutput("hasData", (dirs.length > 0));
+      core.setOutput("directoryCount", dirs.length);
+  }else{
+    core.setOutput("hasData", false);
+  }
 
 
   
